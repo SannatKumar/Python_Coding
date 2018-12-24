cb4611ef49b47e6de7f82cab63a3d2279aae8c09
Sannat <rz_kumr@yahoo.com>
2018-12-24 20:38:48 +0200

    Comaprison opeartors in Python

---
+++ comparing/boolean_comparison.py
@@ -0,0 +1,68 @@
+#This program compares things and prints the boolean value
+
+#Boolean refers to the value True or Fale
+# below comparison produce False result and the relut is stored in variable name boolean_value
+#next line simply prints it
+
+"""*** Things I learned here.Putting print after input produces "None" prompt word before user gets to input
+"""
+first_number = input("Enter your first value to compare: ") 
+second_number = input("Enter your second value to compare: ")
+third_number = input("Enter your third Number: ")
+fourth_number = input("Enter your fourth Number: ")
+
+#check greater than is True
+
+boolean_value = first_number > second_number
+
+print(boolean_value)
+
+#check smaller than is True
+
+boolean_value = first_number < second_number
+
+print(boolean_value)
+
+#check less than or equals to True
+
+boolean_value = first_number <= second_number
+
+print(boolean_value)
+
+#check greater than or equals to True
+
+boolean_value = first_number >= second_number
+
+print(boolean_value)
+
+#check if equals to True
+
+boolean_value = first_number == second_number
+
+print(boolean_value)
+
+#check if not equals to True
+
+boolean_value = first_number != second_number
+
+print(boolean_value)
+
+
+# and opertaor
+
+# This operator gives the result true if both conditions are true
+
+boolean_value = first_number > second_number and third_number > fourth_number
+
+print(boolean_value)
+
+
+# or opertaor
+
+# This operator gives the result true if only conditions is true
+
+boolean_value = first_number > second_number or third_number > fourth_number
+
+print(boolean_value)
+
+
