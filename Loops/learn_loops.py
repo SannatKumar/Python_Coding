ce4cb123032e3db42a2b469eaeddb322b072547c
Sannat <rz_kumr@yahoo.com>
2018-12-25 09:35:42 +0200

    Learning loops range and list

---
+++ Loops/learn_loops.py
@@ -0,0 +1,12 @@
+# This program introduce the loops
+
+#create a list that we have already learned
+
+life = ['Surprise', 'Happy', 'Sad', 'Lonely', 'Bitter', 'Sweet']
+
+def use_insentence(emotions):
+    print("Life is " + emotions + " But maybe not\n")
+
+for emotions in life:
+    use_insentence(emotions)
+    print("Learning to live makes more sense than complaining\n")
---
+++ Loops/range_context_learn_loop.py
@@ -0,0 +1,9 @@
+# This Python program helps to learn loops using ranges
+
+for i in range(1,100):
+    if i%2==0:
+        print("Even Numbers:",i)
+    else:
+        print("Odd Numbers:",i)
+    
+    
